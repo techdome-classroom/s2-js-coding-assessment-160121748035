4943834d88b8bbec6bac1fa6a84a02054d4d6d9b
srikar <srikarrao2021@gmail.com>
2024-11-11 15:12:23 +0530

    Completed program1 and program2

--- program1.js
+++ program1.js
@@ -3,7 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let map = {'(' : ')', '[': ']', '{':'}'};
+    let stack = [];
+    for(let ch of s){
+        if(map[ch]){
+		//pushing opening bracket first
+            stack.push(map[ch]);
+        }
+        else if(stack.length > 0 && stack[stack.length-1] == ch){
+		//if we have a matching closing bracket to the one we pushed earlier, we simply pop it.
+            stack.pop();
+        }
+        else return false;
+    }
+	//if at last all values are poped i.e stack is empty, we have valid set of parentheses.
+    return stack.length == 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -3,6 +3,28 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    let table = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        //if the next roman numeral is larger, then we know we have to subtract this number
+        if (table[s[i]] < table[s[i+1]]) {
+            result-=table[s[i]]
+        } 
+        //otherwise, add like normal. 
+        else {
+            result+=table[s[i]]
+        }
+    }
+    return result
     
 };
 
